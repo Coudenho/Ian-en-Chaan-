79c82cb08cfff080517c2e13cfd83ef91276869c
ifastl <i.fastl@student.tudelft.nl>
2021-09-10 15:49:29 +0200

    !

--- power.s
+++ power.s
@@ -1,8 +1,15 @@
+.data
+name: .asciz "Chaan en Ian, cvandenoudenho en ifastl ,3.10 part A \n"
 .text
+
 .global main
 main:
-	pushq %ebp
-	movq %esp, %ebp
+//prologue
+	push %rbp
+	mov  %rsp, %rbp  //nieuwe stuk stack, beide op zelfde niveau beginne
 
-	movq $1, %eax
-	int 0x80
+	movq $0, %rax    
+	movq $name, %rdi
+	call printf
+	movl $1, %eax
+	int $0x80
