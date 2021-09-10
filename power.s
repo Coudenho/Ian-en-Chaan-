bb2224d45c05ecc92220f1a500aa36d7ca9cf324
ifastl <i.fastl@student.tudelft.nl>
2021-09-10 15:54:05 +0200

    Test run 2

--- power.s
+++ power.s
@@ -1,13 +1,14 @@
 .data
-name: .asciz "Chaan \n"
+name: .asciz "Chaan en Ian, cvandenoudenho en ifastl ,3.10 part A \n"
 .text
 
 .global main
 main:
+//prologue
 	push %rbp
-	mov  %rsp, %rbp
+	mov  %rsp, %rbp  //nieuwe stuk stack, beide op zelfde niveau beginne
 
-	movq $0, %rax
+	movq $0, %rax    
 	movq $name, %rdi
 	call printf
 	movl $1, %eax
