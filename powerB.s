f0f91ad1ebbe1467f011b820900b963bb5e33f4c
ifastl <i.fastl@student.tudelft.nl>
2021-09-20 20:46:41 +0200

    .

--- powerB.s
+++ powerB.s
@@ -1,5 +1,5 @@
 .data
-enter: .asciz "Enter first a base and than an exponent: \n"
+enter: .asciz "Enter first an EXPONENT and than a BASE: \n"
 formatstr: .asciz "%ld%ld"
 result: .asciz "the result is: %ld \n"
 .text
@@ -14,13 +14,18 @@ main:
 	call printf	    #print what is in rdi
 
     subq $16, %rsp          #reserve stack space
-    leaq -16(%rbp), %rsi    #first input on -16rbp and store that adress in rsi
-    leaq -8(%rbp), %rdx     #first input on -16rbp and store that adress in rsi
-    movq $formatstr, %rdi   #define our form of input 
+    
+ 
     movq $0, %rax           #clear all vectors
+    leaq -8(%rbp), %rsi    #first input on -16rbp and store that adress in rsi
+    leaq -16(%rbp), %rdx     #first input on -16rbp and store that adress in rsi
+    movq $formatstr, %rdi   #define our form of input
     call scanf              #call the scanf function
+ 
+    movq -8(%rbp), %rdi
+    movq -16(%rbp), %rsi
 
-    call power_loop_start   #call the subroutine
+    call pow   #call the subroutine
     movq %rax, %rsi         #move rax/result to rsi
     movq $0, %rax           #clear all vectors = 0
     movq $result, %rdi      #move the the result intro rdi, string result contains 1 integer which is rsi
@@ -30,23 +35,28 @@ main:
     popq %rbp
     call exit
 
-power_loop_start:
+pow:
 pushq %rbp                  #prologue (new start of stack)
 movq %rsp, %rbp             #prologue               
-movq 16(%rbp), %rdx         #move second input to rdx
-movq 24(%rbp), %rcx         #move first input to rcx
-movq %rdx, -8(%rbp)         #move rdx/first input to current top of stack
+pushq %rdi       #move rdx/first input to current top of stack
+subq $8, %rsp
 
 
 power_loop:
 
-    cmpq $1, %rcx  # check if the base has been multiplied with itself (exponent -1) times
+    cmpq $1, %rsi  # check if the base has been multiplied with itself (exponent -1) times
     je end             # if it has, end the loop
+    cmpq $1, %rsi
+    jl if_zero
     movq -8(%rbp), %rax  #move first input (base) into rax
-    imulq %rdx , %rax     #multiply rdx (which is the base) with rax and store it in rax
+    imulq %rdi , %rax     #multiply rdx (which is the base) with rax and store it in rax
     movq %rax, -8(%rbp)   #move rax/outcome of the multiplication to what was top of stack/ -8rbp
-    decq %rcx             # decrement the exponent (second input, that is stored in rcx)
+    decq %rsi             # decrement the exponent (second input, that is stored in rcx)
     jmp power_loop      # go back to the beginning of the loop 
+
+if_zero:
+    movq $1, -8(%rbp)
+
 end:
     movq -8(%rbp), %rax  #move the result/top of stack into rax
     movq %rbp, %rsp
--- retest.s
+++ retest.s
@@ -1,5 +1,5 @@
 .data
-enter: .asciz "Enter first a base and than an exponent: \n"
+enter: .asciz "Enter first an EXPONENT and than a BASE: \n"
 formatstr: .asciz "%ld%ld"
 result: .asciz "the result is: %ld \n"
 .text
@@ -9,50 +9,56 @@ main:
    pushq %rbp               # prologue
    movq %rsp, %rbp
 
-    movq $0, %rax    
-	movq $enter, %rdi
-	call printf
+    movq $0, %rax           #Clear all vectors
+	movq $enter, %rdi   #move enter into rdi
+	call printf	    #print what is in rdi
 
-    subq $16, %rsp
-    leaq -16(%rbp), %rsi
-    leaq -8(%rbp), %rdx
-    movq $formatstr, %rdi
-    movq $0, %rax
-    call scanf
-
-    call power_loop_start
-    movq %rax, %rsi
-    movq $0, %rax
-    movq $result, %rdi
-    call printf
+    subq $16, %rsp          #reserve stack space
+    
+ 
+    movq $0, %rax           #clear all vectors
+    leaq -8(%rbp), %rsi    #first input on -16rbp and store that adress in rsi
+    leaq -16(%rbp), %rdx     #first input on -16rbp and store that adress in rsi
+    movq $formatstr, %rdi   #define our form of input
+    call scanf              #call the scanf function
+ 
+    movq -8(%rbp), %rdi
+    
+    call power_loop_start   #call the subroutine
+    movq %rax, %rsi         #move rax/result to rsi
+    movq $0, %rax           #clear all vectors = 0
+    movq $result, %rdi      #move the the result intro rdi, string result contains 1 integer which is rsi
+    call printf             #print what is in rdi
    
-    // movl $1, %eax
-    // int $0x80
-
     movq %rbp, %rsp
     popq %rbp
     call exit
 
 power_loop_start:
-pushq %rbp
-movq %rsp, %rbp
-subq $8, %rsp
-movq 16(%rbp), %rdx
-movq 24(%rbp), %rcx 
-movq %rdx, -8(%rbp)
+pushq %rbp                  #prologue (new start of stack)
+movq %rsp, %rbp             #prologue               
+movq %rsi, -8(%rbp)         #move rdx/first input to current top of stack
 
 
 power_loop:
 
-    cmpq $1, %rcx  # check if the base has been multiplied with itself (exponent -1) times
+    cmpq $1, %rdi  # check if the base has been multiplied with itself (exponent -1) times
     je end             # if it has, end the loop
-    movq -8(%rbp), %rax
-    imulq %rdx, %rax
-    movq %rax, -8(%rbp)
-    decq %rcx        # decrement the exponent
+    cmpq $1, %rdi
+    jl if_zero
+    movq -8(%rbp), %rax  #move first input (base) into rax
+    imulq %rsi , %rax     #multiply rdx (which is the base) with rax and store it in rax
+    movq %rax, -8(%rbp)   #move rax/outcome of the multiplication to what was top of stack/ -8rbp
+    decq %rdi             # decrement the exponent (second input, that is stored in rcx)
     jmp power_loop      # go back to the beginning of the loop 
+
+if_zero:
+    movq $1, -8(%rbp)
+
 end:
-    movq -8(%rbp), %rax
+    movq -8(%rbp), %rax  #move the result/top of stack into rax
     movq %rbp, %rsp
     popq %rbp
-    ret
+    ret                  #return to main
+
+    #I made a sort of drawing of the stack of this code
